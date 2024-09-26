57c8072d45417960c4370094685844eed8c18887
jlandgre <jdlandgrebe@icloud.com>
2024-09-26 11:55:58 -0400

    Validated survey case study; changes through 9/26 11:55

--- libs/projtables.py
+++ libs/projtables.py
@@ -1,6 +1,7 @@
-#Version 9/24/24 JDL
+#Version 9/26/24 JDL
 import os, sys
 import pandas as pd
+import numpy as np
 from openpyxl import load_workbook
 
 path_libs = os.getcwd() + os.sep + 'libs' + os.sep
@@ -40,12 +41,12 @@ class ProjectTables():
         self.pf_input3 = ''
 
         self.Table1 = Table(self.pf_input1, 'Table1', 'raw_table', 'idx')
-        self.Table2 = Table(self.pf_input2, 'Table2', 'first_sheet', 'idx')
-        self.Table3 = Table(self.pf_input2, 'Table3', 'second_sheet', 'idx')
+        self.Table2 = Table(self.pf_input1, 'Table2', 'first_sheet', 'idx')
+        self.Table3 = Table(self.pf_input1, 'Table3', 'second_sheet', 'idx')
 
         #Set lists of inputs and outputs
-        self.lstImports = [self.Table2]
-        self.lstRawImports = [self.Table1]
+        self.lstImports = [self.Table2] #structured Excel data imported to tbl.df
+        self.lstRawImports = [self.Table1] #unstructured Excel data to tbl.df_raw
         self.lstOutputs = []
 
         #Initialize Output DataFrames
@@ -76,12 +77,11 @@ class ProjectTables():
         tbl.dParseParams['col_last_df'] to specify where to truncate columns
         JDL refactored 9/3/24
         """
-        print('\nin import', self.lstImports)
         for tbl in self.lstImports:
             tbl.ImportExcelDf()
 
             if self.IsPrint:
-                print('\nImported', tbl.name, tbl.pf, tbl.sht)
+                print('\nImported Excel', tbl.name, tbl.pf, tbl.sht)
                 print(tbl.df)
     
     def ImportRawInputs(self):
@@ -91,33 +91,30 @@ class ProjectTables():
         JDL 3/4/24; Modified 9/25/24
         """
         for tbl in self.lstRawImports:
+            tbl.ImportExcelRaw()
 
-            #Create workbook object and select sheet
-            wb = load_workbook(filename=tbl.pf, read_only=True)
-            ws = wb[tbl.sht]
-
-            # Convert the data to a list and convert to a DataFrame
-            data = ws.values
-            tbl.df_raw = pd.DataFrame(data)
+            if self.IsPrint:
+                print('\nImported Excel Raw', tbl.name, tbl.pf, tbl.sht)
+                print(tbl.df)
 
 class Table():
     """
     Attributes for a data table including import instructions and other
     metadaeta. Table instances are attributes of ProjectTables Class
-    JDL Modified 8/27/24 add _cols list attribute initialization
+    JDL Modified 9/26/24 add import_dtype argument
     """
-    def __init__(self, pf, name, sht, idx_col_name, dParseParams=None):
+    def __init__(self, pf, name, sht, idx_col_name, dParseParams=None, import_dtype=None):
                 
         #Import info: Path+File (sPF), Excel sheet name for import
         self.pf = pf
         self.sht = sht
 
-        #Optional dictionary of parsing parameters for .df or .df_raw
-        self.dParseParams = dParseParams
-
         #Table name (string) and name of default index column
         self.name = name #Table name
         self.idx_col_name = idx_col_name
+        self.dParseParams = dParseParams
+        self.import_col_map = {} #Map raw import names to df col names
+        self.import_dtype = import_dtype #To force str type for imported values
 
         #Raw (non-parsed) and parsed DataFrames
         self.df_raw = pd.DataFrame()
@@ -144,6 +141,25 @@ class Table():
                 self.df = self.df.iloc[:, :idx_last+1]
             except KeyError:
                 raise ValueError(f"Column {col_last} not found in", self.name)
+
+    def ImportExcelRaw(self):
+        """
+        Import unstructured data to .df_raw for parsing
+        JDL Modified 9/26/24 to allow forcing str type for imported values
+        """
+        #Create workbook object and select sheet
+        wb = load_workbook(filename=self.pf, read_only=True)
+        ws = wb[self.sht]
+
+        # Convert the data to a list and convert to a DataFrame
+        data = ws.values
+        self.df_raw = pd.DataFrame(data)
+
+        #Negate Pandas inferring float data type for integers and NaNs for blanks
+        if self.import_dtype == str:
+            self.df_raw = self.df_raw.applymap(lambda x: None if pd.isna(x) \
+                    else str(int(x)) if isinstance(x, float) and x.is_integer() \
+                    else str(x))
 
     def ResetDefaultIndex(self, IsDrop=True):
         """
@@ -180,10 +196,10 @@ class RowMajorTbl():
     (imported with tbls.ImportInputs() or .ImportRawInputs() methods
     JDL 3/4/24
     """
-    def __init__(self, dParseParams, tbl):
+    def __init__(self, tbl):
 
         #Parsing params (inputs and found during parsing)
-        self.dParseParams = dParseParams
+        #self.dParseParams = tbl.dParseParams
 
         #List of df indices where flag_start_bound is found
         self.start_bound_indices = []
@@ -197,6 +213,7 @@ class RowMajorTbl():
 
         #Current block start row index (in loop procedure)
         self.idx_start_current = None
+        self.idx_header_row = None
 
         #Temporary storage of block's parsed data
         self.df_block = pd.DataFrame()
@@ -206,6 +223,10 @@ class RowMajorTbl():
         Procedure to find flag_start_bound's and iteratively parse blocks
         JDL 9/25/24
         """
+
+        # Append a blank row at the end of self.df_raw (to ensure find last <blank> flag)
+        self.AddTrailingBlankRow()
+
         #Create list of row indices with start bound flag
         self.SetStartBoundIndices()
 
@@ -220,13 +241,22 @@ class RowMajorTbl():
         #Optionally stack parsed data (if .dParams['is_stack_parsed_cols']
         self.StackParsedCols()
 
+    def AddTrailingBlankRow(self):
+        """
+        Add a trailing blank row to self.df_raw (to ensure last <blank> flag to
+        terminate last block)
+        JDL 9/26/24
+        """
+        blank_row = pd.Series([np.nan] * len(self.df_raw.columns), index=self.df_raw.columns)
+        self.df_raw = pd.concat([self.df_raw, pd.DataFrame([blank_row])], ignore_index=True)
+
     def SetStartBoundIndices(self):
         """
         Populate list of row indices whereflag_start_bound is found
         JDL 9/25/24
         """
-        flag= self.dParseParams['flag_start_bound']
-        icol = self.dParseParams['icol_start_bound']
+        flag= self.tbl.dParseParams['flag_start_bound']
+        icol = self.tbl.dParseParams['icol_start_bound']
 
         fil = self.df_raw.iloc[:, icol] == flag
         self.start_bound_indices = self.df_raw[fil].index.tolist()
@@ -236,7 +266,6 @@ class RowMajorTbl():
         Set the table's default index
         JDL 3/4/24
         """
-        print('\ndefault index', self.tbl.idx_col_name)
         self.tbl.df = self.tbl.df.set_index(self.tbl.idx_col_name)
     
     def StackParsedCols(self):
@@ -244,7 +273,7 @@ class RowMajorTbl():
         Optionally stack parsed columns from row major blocks
         JDL 9/25/24
         """
-        is_stack = self.dParseParams.get('is_stack_parsed_cols', False)
+        is_stack = self.tbl.dParseParams.get('is_stack_parsed_cols', False)
 
         if is_stack:
             self.tbl.df = self.tbl.df.stack().reset_index()
@@ -273,18 +302,19 @@ class RowMajorTbl():
         Find index of flag_end_bound
         JDL 3/4/24; modified 9/25/24
         """
-        flag, icol = self.dParseParams['flag_end_bound'], self.dParseParams['icol_end_bound']
+        flag, icol = self.tbl.dParseParams['flag_end_bound'], self.tbl.dParseParams['icol_end_bound']
 
         #Start the search at the first data row based on data offset from flag
         idx_start = self.idx_start_current + \
-            self.dParseParams['idata_rowoffset_from_flag']
+            self.tbl.dParseParams['idata_rowoffset_from_flag']
 
         # if flag string indicates search for first null
         if flag == '<blank>':
             idx_end_bound = self.df_raw.iloc[idx_start:, icol].isnull().idxmax()
         else:
             idx_end_bound = self.df_raw.iloc[idx_start:, icol].eq(flag).idxmax()
-        self.dParseParams['idx_end_bound'] = idx_end_bound
+
+        self.tbl.dParseParams['idx_end_bound'] = idx_end_bound
 
     def ReadHeader(self):
         """
@@ -292,37 +322,45 @@ class RowMajorTbl():
         JDL 3/4/24; modified 9/25/24
         """
         # Calculate the header row index
-        #idx_start = self.dParseParams['idx_start_bound']
-        iheader_offset = self.dParseParams['iheader_rowoffset_from_flag']
-        idx_header_row =  self.idx_start_current + iheader_offset
+        #idx_start = self.tbl.dParseParams['idx_start_bound']
+        iheader_offset = self.tbl.dParseParams['iheader_rowoffset_from_flag']
+        self.idx_header_row =  self.idx_start_current + iheader_offset
 
-        # Set the column names
-        self.lst_df_raw_cols = list(self.df_raw.iloc[idx_header_row])
-        self.dParseParams['idx_header_row'] = idx_header_row
+        # Set the column names (drop columns with blank header)
+        self.cols_df_block = \
+            self.df_raw.iloc[self.idx_header_row].values
+                
+        #self.lst_df_raw_cols = list(self.df_raw.iloc[idx_header_row])
+        #self.tbl.dParseParams['idx_header_row'] = idx_header_row
 
     def SubsetDataRows(self):
         """
         Subset rows based on flags and idata_rowoffset_from_flag.
-        JDL 3/4/24
+        JDL 3/4/24; Modified 9/25/24
         """
         # Calculate the start index for the data
         idx_start_data = self.idx_start_current + \
-            self.dParseParams['idata_rowoffset_from_flag']
-        idx_end_bound = self.dParseParams['idx_end_bound']
+            self.tbl.dParseParams['idata_rowoffset_from_flag']
+        idx_end_bound = self.tbl.dParseParams['idx_end_bound']
 
         # Subset the data rows and set columns
         self.df_block = self.df_raw.iloc[idx_start_data:idx_end_bound]
-        self.df_block.columns = self.lst_df_raw_cols
 
     def SubsetCols(self):
         """
         Use tbl.import_col_map to subset columns based on header.
-        JDL 9/24/24
+        JDL 9/25/24
         """
+        self.df_block.columns = self.cols_df_block
+
         #Use import_col_map if specified
         if len(self.tbl.import_col_map) > 0:
             cols_keep = list(self.tbl.import_col_map.keys())
             self.df_block = self.df_block[cols_keep]
+        
+        #Drop columns with blank (e.g. NaN) header
+        else:
+            self.df_block = self.df_block.dropna(axis=1, how='all')
 
     def RenameCols(self):
         """
--- tests/test_parsetables.py
+++ tests/test_parsetables.py
@@ -1,4 +1,4 @@
-#Version 9/25/24
+#Version 9/26/24
 #python -m pytest test_parsetables.py -v -s
 import sys, os
 import pandas as pd
@@ -14,6 +14,7 @@ if not path_libs in sys.path: sys.path.append(path_libs)
 print('\n', path_libs)
 from projfiles import Files
 from projtables import ProjectTables
+from projtables import Table
 from projtables import RowMajorTbl
 
 """
@@ -34,23 +35,9 @@ RowMajorTbl Class - for parsing row major raw data
 ================================================================================
 """
 @pytest.fixture
-def tbls_survey(files):
-    """
-    Using .ImportRawInputs() method to import sheet whose data may not start at A1
-    """
-    tbls = ProjectTables(files, ['tbl1_survey.xlsx'])
-    tbls.ImportRawInputs()
-
-    #Override hard-coded demo for tbls.Table1
-    tbls.Table1.import_col_map = {}
-    tbls.Table1.idx_col_name = 'Answer Choices'
-
-    return tbls
-
-@pytest.fixture
 def dParseParams_tbl1_survey():
     """
-    Return a dictionary of parameters for parsing the first table
+    Dictionary of parameters for parsing survey data
     """
     dParseParams = {}
     dParseParams['flag_start_bound'] = 'Answer Choices'
@@ -62,12 +49,24 @@ def dParseParams_tbl1_survey():
     return dParseParams
 
 @pytest.fixture
-def row_maj_tbl1_survey(tbls_survey, dParseParams_tbl1_survey):
+def tbl1_survey(files, dParseParams_tbl1_survey):
+    """
+    Table object for survey data
+    JDL 9/25/24
+    """
+    pf = files.path_data + 'tbl1_survey.xlsx'
+    tbl = Table(pf, 'Table1', 'raw_table', 'Answer Choices', \
+                dParseParams_tbl1_survey, import_dtype=str)
+    tbl.ImportExcelRaw()
+    return tbl
+
+@pytest.fixture
+def row_maj_tbl1_survey(tbl1_survey):
     """
     Instance RowMajorTbl parsing class for survey data
-    JDL 9/24/24
+    JDL 9/25/24
     """
-    return RowMajorTbl(dParseParams_tbl1_survey, tbls_survey.Table1)
+    return RowMajorTbl(tbl1_survey)
 
 """
 ================================================================================
@@ -80,8 +79,16 @@ def test_survey_ReadBlocksProcedure1(row_maj_tbl1_survey):
     """
     row_maj_tbl1_survey.ReadBlocksProcedure()
 
-    #Check that procedure found two blocks
-    row_maj_tbl1_survey.start_bound_indices = [3, 14]
+    #Check that procedure found three blocks
+    assert row_maj_tbl1_survey.start_bound_indices == [3, 14, 24]
+    assert len(row_maj_tbl1_survey.tbl.df) == 11
+
+    df_check = row_maj_tbl1_survey.tbl.df.reset_index(drop=False)
+
+    lst_expected = ['Daily', '14.13%', '76', np.nan, np.nan, np.nan]
+    check_series_values(df_check.iloc[0], lst_expected)
+    lst_expected =  ['Improved cleaning', np.nan, np.nan, '18', '11', '17']
+    check_series_values(df_check.iloc[-1], lst_expected)
 
     if True: print_tables(row_maj_tbl1_survey)
 
@@ -92,28 +99,62 @@ def test_survey_ReadBlocksProcedure2(row_maj_tbl1_survey):
     Stack the parsed data
     JDL 9/25/24
     """
+    row_maj_tbl1_survey.tbl.dParseParams['is_stack_parsed_cols'] = True
     row_maj_tbl1_survey.ReadBlocksProcedure()
 
-    #Check that procedure found two blocks
-    row_maj_tbl1_survey.start_bound_indices = [3, 14]
+    assert len(row_maj_tbl1_survey.tbl.df) == 25
 
-    row_maj_tbl1_survey.dParseParams['is_stack_parsed_cols'] = True
-    row_maj_tbl1_survey.StackParsedCols()
+    # Check values in first two and last two rows
+    df_check = row_maj_tbl1_survey.tbl.df.reset_index(drop=False)
+    lst_expected = ['Daily', 'Response Percent', '14.13%']
+    check_series_values(df_check.iloc[0], lst_expected)
+    lst_expected = ['Daily', 'Responses', '76']
+    check_series_values(df_check.iloc[1], lst_expected)
+
+    lst_expected =  ['Improved cleaning', '3', '17']
+    check_series_values(df_check.iloc[-1], lst_expected)
+    lst_expected =  ['Improved cleaning', '2', '11']
+    check_series_values(df_check.iloc[-2], lst_expected)
 
     if True: print_tables(row_maj_tbl1_survey)
 
-def test_survey_ParseBlockProcedure(row_maj_tbl1_survey):
+def test_survey_ParseBlockProcedure1(row_maj_tbl1_survey):
     """
     Parse the survey table and check the final state of the table.
+    (1st block)
     JDL 9/25/24
     """
+
     SetListFirstStartBoundIndex(row_maj_tbl1_survey)
     row_maj_tbl1_survey.ParseBlockProcedure()
 
     #Check resulting .tbl.df relative to tbl1_survey.xlsx
     assert len(row_maj_tbl1_survey.tbl.df) == 5
-    assert list(row_maj_tbl1_survey.tbl.df.iloc[0]) == ['Daily', '14.13%', 76]
-    assert list(row_maj_tbl1_survey.tbl.df.iloc[-1]) == ['Rarely', '0.37%', 2]
+    assert list(row_maj_tbl1_survey.tbl.df.iloc[0]) == ['Daily', '14.13%', '76']
+    assert list(row_maj_tbl1_survey.tbl.df.iloc[-1]) == ['Rarely', '0.37%', '2']
+
+    if False: print_tables(row_maj_tbl1_survey)
+
+def test_survey_ParseBlockProcedure2(row_maj_tbl1_survey):
+    """
+    Parse the survey table and check the final state of the table.
+    JDL 9/25/24; Modified 9/26/24
+    """
+    #Add a trailing blank row to .df_raw (last <blank> end flag)
+    row_maj_tbl1_survey.AddTrailingBlankRow()
+
+    #Set current start bound index to be last (third) block
+    row_maj_tbl1_survey.SetStartBoundIndices()
+    row_maj_tbl1_survey.idx_start_current = \
+        row_maj_tbl1_survey.start_bound_indices[-1]
+    assert row_maj_tbl1_survey.idx_start_current == 24
+
+    row_maj_tbl1_survey.ParseBlockProcedure()
+
+    #Check resulting .tbl.df relative to tbl1_survey.xlsx
+    assert len(row_maj_tbl1_survey.tbl.df) == 3
+    assert list(row_maj_tbl1_survey.tbl.df.iloc[0]) == ['Lower price point', '91', '33', '19']
+    assert list(row_maj_tbl1_survey.tbl.df.iloc[-1]) == ['Improved cleaning', '18', '11', '17']
 
     if False: print_tables(row_maj_tbl1_survey)
 
@@ -152,8 +193,11 @@ def test_survey_SubsetDataRows(row_maj_tbl1_survey):
 
     # Check resulting .tbl.df relative to tbl1_raw.xlsx
     assert len(row_maj_tbl1_survey.df_block) == 5
-    assert list(row_maj_tbl1_survey.df_block.iloc[0]) == ['Daily', '14.13%', 76]
-    assert list(row_maj_tbl1_survey.df_block.iloc[-1]) == ['Rarely', '0.37%', 2]
+    lst_expected = ['Daily', '14.13%', '76', None, None, None]
+    check_series_values(row_maj_tbl1_survey.df_block.iloc[0], lst_expected)
+
+    lst_expected = ['Rarely', '0.37%', '2', None, None, None]
+    check_series_values(row_maj_tbl1_survey.df_block.iloc[-1], lst_expected)
 
 def test_survey_ReadHeader(row_maj_tbl1_survey):
     """
@@ -165,11 +209,22 @@ def test_survey_ReadHeader(row_maj_tbl1_survey):
     row_maj_tbl1_survey.ReadHeader()
 
     # Assert that the header row index was set correctly
-    assert row_maj_tbl1_survey.dParseParams['idx_header_row'] == 3
+    assert row_maj_tbl1_survey.idx_header_row == 3
 
-    # Assert that the column names were read correctly
-    lst_expected = ['Answer Choices', 'Response Percent', 'Responses']
-    assert row_maj_tbl1_survey.lst_df_raw_cols == lst_expected
+    #  Check each column name, allowing for NaN comparisons
+    lst_expected = ['Answer Choices', 'Response Percent', 'Responses', None, None, None]
+    check_series_values(row_maj_tbl1_survey.cols_df_block, lst_expected)
+
+def check_series_values(ser, lst_expected):
+    """
+    Helper function to check series values allowing for NaN comparisons
+    JDL 9/25/24
+    """
+    for actual, expect in zip(ser, lst_expected):
+        if isinstance(expect, float) and np.isnan(expect):
+            assert np.isnan(actual)
+        else:
+            assert actual == expect
 
 def test_survey_FindFlagEndBound(row_maj_tbl1_survey):
     """
@@ -181,7 +236,7 @@ def test_survey_FindFlagEndBound(row_maj_tbl1_survey):
     assert row_maj_tbl1_survey.idx_start_current == 3
 
     row_maj_tbl1_survey.FindFlagEndBound()
-    assert row_maj_tbl1_survey.dParseParams['idx_end_bound'] == 9
+    assert row_maj_tbl1_survey.tbl.dParseParams['idx_end_bound'] == 9
 
 def SetListFirstStartBoundIndex(row_maj_tbl1_survey):
     """
@@ -201,16 +256,33 @@ def test_survey_SetStartBoundIndices(row_maj_tbl1_survey):
     row_maj_tbl1_survey.SetStartBoundIndices()
 
     # Expected indices where 'Answer Choices' is found
-    expected_indices = [3, 14]
+    expected_indices = [3, 14, 24]
 
     assert row_maj_tbl1_survey.start_bound_indices == expected_indices
 
-def test_survey_tbls_fixture(tbls_survey):    
+def test_survey_AddTrailingBlankRow(row_maj_tbl1_survey):
     """
-    Test that the tbl1_survey.xlsx was imported correctly
+    Add a trailing blank row to self.df_raw (to ensure last <blank> flag to
+    terminate last block)
+    JDL 9/26/24
+    """
+    assert row_maj_tbl1_survey.df_raw.shape == (28, 4)
+    row_maj_tbl1_survey.AddTrailingBlankRow()
+    assert row_maj_tbl1_survey.df_raw.shape == (29, 4)
+
+def test_survey_row_maj_fixture(row_maj_tbl1_survey):    
+    """
+    Test that raw survey data imported correctly
     JDL 9/24/24
     """
-    assert tbls_survey.Table1.df_raw.shape == (20, 3)
+    assert row_maj_tbl1_survey.tbl.df_raw.shape == (28, 4)
+
+def test_survey_tbl1_fixture(tbl1_survey):
+    """
+    Test that survey data imported correctly
+    JDL 9/25/24
+    """
+    assert tbl1_survey.df_raw.shape == (28, 4)
 
 """
 ================================================================================
